24796eac3150c5b778c1319f31bd1acc5e84ef38
Kevin_FGR <kevinfelipe.gr@gmail.com>
2025-11-16 00:27:47 -0300

    feat: change list design

--- perinvest_app/lib/app/pages/cryptos/cryptos.controller.dart
+++ perinvest_app/lib/app/pages/cryptos/cryptos.controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:perinvest_app/app/pages/cryptos/create/cryptos.create.page.dart';
+import 'package:perinvest_app/app/pages/cryptos/form/cryptos.form.page.dart';
+import 'package:perinvest_app/helpers/color.helper.dart';
 import 'package:perinvest_app/helpers/toast.helper.dart';
 import 'package:perinvest_app/services/cryptos.service.dart';
 
@@ -18,7 +19,7 @@ class CryptosController extends ChangeNotifier{
   static void goToCreate(BuildContext context) {
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(builder: (context) => const CryptosCreatePage()),
+      MaterialPageRoute(builder: (context) => const CryptosFormPage()),
     );
   }
 
@@ -41,40 +42,43 @@ class CryptosController extends ChangeNotifier{
       cryptosList.add(SizedBox(
         width: double.infinity,
         child: GestureDetector(
-          onTap: () => callbackPage?.call(CryptosCreatePage()),
-          // onTap: () => CryptosController.goToCreate(context),
+          onTap: () => callbackPage?.call(CryptosFormPage()),
           child: Container(
             decoration: BoxDecoration(
-              color: Colors.grey.shade900,
+              color: ColorHelper.darkLight,
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
-                end: Alignment.centerLeft,
+                end: Alignment.centerRight,
                 colors: [
-                  Colors.grey.shade900,
-                  Colors.amber.shade900,
+                  ColorHelper.darkLight,
+                  ColorHelper.primary,
+                  ColorHelper.primaryLight,
+                  ColorHelper.primary,
+                  ColorHelper.darkLight,
                 ],
-                stops: [0.8, 0.99]
+                // stops: [0.8, 0.95, 0.99]
+                stops: [0.55, 0.60, 0.65, 0.7, 0.99]
               ),
               borderRadius: BorderRadius.circular(10),
               border: Border.all(
-                color: Colors.amber[900]!,
+                color: ColorHelper.primary.withValues(alpha:0.7),
                 width: 0.5,             
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.amber.shade900.withValues(alpha:0.3),
-                  spreadRadius: 1,
-                  blurRadius: 3,
-                  offset: const Offset(0, 0)
-                ),
-              ],
+              // boxShadow: [
+              //   BoxShadow(
+              //     color: ColorHelper.primary.withValues(alpha:0.3),
+              //     spreadRadius: 1,
+              //     blurRadius: 3,
+              //     offset: const Offset(0, 0)
+              //   ),
+              // ],
             ),
             padding: const EdgeInsets.all(15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(cryptos[i]['description']),
-                const Icon(Icons.arrow_forward_ios, size: 15, color: Colors.grey),
+                const Icon(Icons.arrow_forward_ios, size: 15),
               ]
             )
           )
