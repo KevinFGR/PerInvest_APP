46e55e47464b9ab038dcafcb78555d53be088083
Kevin_FGR <kevinfelipe.gr@gmail.com>
2025-11-23 20:20:55 -0300

    feat: danger at collorHelper

--- perinvest_app/lib/helpers/color.helper.dart
+++ perinvest_app/lib/helpers/color.helper.dart
@@ -4,4 +4,5 @@ class ColorHelper{
   static Color primary = Colors.amber.shade900;
   static Color primaryLight = Colors.amber.shade500;
   static Color darkLight = Colors.grey.shade900;
+  static Color danger = Colors.red.shade500;
 }
